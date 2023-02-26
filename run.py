6602e012e53a59f40961f162d82b93e5203e84ee
Jon Craton <jncraton@gmail.com>
2023-02-26 00:36:15 -0500

    Limit benchmark size

--- run.py
+++ run.py
@@ -1,7 +1,25 @@
 import lm_eval
+import json
 
-model = lm_eval.get_model("hf-causal", pretrained="distilgpt2", device="cpu")
 tasks = lm_eval.get_task_list(
     "boolq",
     template_names=['yes_no_question'])
-results = lm_eval.evaluate(model=model, tasks=tasks)
+
+tasks[0].dataset = tasks[0].dataset.filter(lambda _, i: i < 32, with_indices=True)
+def bench(model_name):
+    if 't5' in model_name:
+        model = lm_eval.get_model("hf-seq2seq", pretrained=model_name, device="cpu")
+    else:
+        model = lm_eval.get_model("hf-causal", pretrained=model_name, device="cpu")
+
+    results = lm_eval.evaluate(model=model, tasks=tasks)
+    print(results)
+
+    with open(f"results/{model_name.replace('/','-')}.json", 'w') as out:
+        out.write(json.dumps(results))
+
+if __name__ == '__main__':
+    bench('google/flan-t5-small')
+    bench('google/flan-t5-base')
+    bench('distilgpt2')
+    bench('gpt2')
