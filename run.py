9f659614c0f6126706a4b1c38a1736ccb2df803f
Jon Craton <jncraton@gmail.com>
2023-03-07 15:16:28 -0500

    Support fewshot tasks

--- run.py
+++ run.py
@@ -21,24 +21,28 @@ task_config = {
 for task in task_config.values():
     task[0].dataset = task[0].dataset.filter(lambda _, i: i < max_checks, with_indices=True)
 
-def bench(model_name, tasks=["cb", "copa", "boolq"]):
+def bench(model_name, tasks=["cb", "copa", "boolq"], shotlist=[0]):
     for task in tasks:
-        outfile = f"results/{model_name.replace('/','-')}-{task}.json"
+        for shots in shotlist:
+            if shots > 0:
+                outfile = f"results/{model_name.replace('/','-')}-{task}-{shots}.json"
+            else:
+                outfile = f"results/{model_name.replace('/','-')}-{task}.json"
 
-        if (os.path.isfile(outfile)):
-            print(f"{task} for {model_name} already complete ({outfile})")
-            return
+            if (os.path.isfile(outfile)):
+                print(f"{task} for {model_name} ({shots}-shot) already complete ({outfile})")
+                continue
 
-        model_type = 'hf-seq2seq' if 't5' in model_name or 'bart' in model_name or 'tk' in model_name or 't0' in model_name else 'hf-causal'
+            model_type = 'hf-seq2seq' if 't5' in model_name or 'bart' in model_name or 'tk' in model_name or 't0' in model_name else 'hf-causal'
 
-        print(f"Running {task} benchmark on {model_name} ({model_type})")
+            print(f"Running {task} benchmark on {model_name} ({model_type})")
 
-        model = lm_eval.get_model(model_type, pretrained=model_name, device='cpu', dtype=torch.float32)
+            model = lm_eval.get_model(model_type, pretrained=model_name, device='cpu', dtype=torch.float32)
 
-        results = lm_eval.evaluate(model=model, tasks=task_config[task], num_fewshot=0)
+            results = lm_eval.evaluate(model=model, tasks=task_config[task], num_fewshot=shots)
 
-        with open(outfile, 'w') as out:
-            out.write(json.dumps(results))
+            with open(outfile, 'w') as out:
+                out.write(json.dumps(results))
 
 if __name__ == '__main__':
     models = list(csv.DictReader(open('open-models.csv')))
